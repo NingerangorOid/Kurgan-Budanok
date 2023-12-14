453fb2cfa7c3524db5966edcd3f84d5ab331bd94
ortus <qwefsdfs@gmail.com>
2023-12-14 18:25:39 +0500

    yandex connect

--- yandexmain.py
+++ yandexmain.py
@@ -1,87 +1,116 @@
-import pygame
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+
+        # основной персонаж
 
 
-pygame.init()
-size = 1000, 700
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Чtрное в белое и наоборот')
-clock = pygame.time.Clock()
+player = None
+
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
 
-class Board:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[1] * width for _ in range(height)]
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
-
-    def render(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                pygame.draw.rect(screen, pygame.Color('green'), (
-                    x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, self.cell_size),
-                                 self.board[y][x])
-        for y in range(self.height):
-            for x in range(self.width):
-                if y >= 1:
-                    if x >= 1:
-                        if self.board[y][x] != self.board[y - 1][x]:
-                            if self.board[y][x] != self.board[y][x - 1]:
-                                if self.board[y][x] != self.board[y + 1][x]:
-                                    if self.board[y][x] != self.board[y][x + 1]:
-                                        self.board[y][x] = (self.board[y][x] + 1) % 2
-                    else:
-                        if self.board[y][x] != self.board[y - 1][x]:
-                            if self.board[y][x] != self.board[y][x + 1]:
-                                if self.board[y][x] != self.board[y + 1][x]:
-                                    self.board[y][x] = (self.board[y][x] + 1) % 2
-                else:
-                    if x >= 1:
-                        if self.board[y][x] != self.board[y + 1][x]:
-                            if self.board[y][x] != self.board[y][x + 1]:
-                                if self.board[y][x] != self.board[y][x - 1]:
-                                    self.board[y][x] = (self.board[y][x] + 1) % 2
-                    else:
-                        if self.board[y][x] != self.board[y + 1][x]:
-                            if self.board[y][x] != self.board[y][x + 1]:
-                                self.board[y][x] = (self.board[y][x] + 1) % 2
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
 
 
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mario.png')
 
-    def on_click_change_cell_color(self, cell_coords):
-        self.board[cell_coords[0]][cell_coords[1]] = (self.board[cell_coords[0]][cell_coords[1]] + 1) % 2
-
-    def get_cell(self, mouse_pos):
-        if self.left <= mouse_pos[1] < self.left + self.height * self.cell_size and self.top <= mouse_pos[
-            0] < self.top + self.width * self.cell_size:
-            return (int((mouse_pos[1] - self.left) / self.cell_size), int((mouse_pos[0] - self.top) / self.cell_size))
-        else:
-            return None
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        if cell != None:
-            self.on_click_change_cell_color(cell)
+tile_width = tile_height = 50
 
 
-board = Board(10, 8)
-board.set_view(10, 10, 40)
-fps = 1
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            board.get_click(event.pos)
-    screen.fill((0, 0, 0))
-    board.render()
-    pygame.display.flip()
-    clock.tick(fps)
-pygame.quit()
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+FPS = 50
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
